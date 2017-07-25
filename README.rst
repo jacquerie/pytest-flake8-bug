b1088ce965a8d0b10497f1318ca5bc7581b38f1d
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2017-07-25 21:30:44 +0200

    global: first commit

---
+++ README.rst
@@ -0,0 +1,8 @@
+=================
+pytest-flake8-bug
+=================
+
+Steps to reproduce:
+
+1. ``$ pip install -e .``
+2. ``$ py.test --flake8 tests``
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+
+setup(
+    name='pytest-flake8-bug',
+    install_requires=[
+        'pytest',
+        'pytest-flake8',
+        'pytest-randomly',
+    ],
+)
